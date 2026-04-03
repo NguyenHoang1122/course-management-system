f20faf1c1c3a6f55a438e1ea978c602d3524c3d7
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2026-04-03 11:40:50 +0700

    list-course

--- src/main/java/com/coursemanagementsystem/controller/CourseController.java
+++ src/main/java/com/coursemanagementsystem/controller/CourseController.java
@@ -1,7 +1,23 @@
 package com.coursemanagementsystem.controller;
 
+import com.coursemanagementsystem.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/courses")
 public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping("")
+    public String findAllCourse(Model model) {
+        model.addAttribute("courses", courseService.findAll());
+        return "course/list";
+    }
+
 }
