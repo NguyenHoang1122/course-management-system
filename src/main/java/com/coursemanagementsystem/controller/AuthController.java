4f61077470303412a0c11132be773681354f6b86
lichnguyen0 <fakesangmy@gmail.com>
2026-04-07 09:04:56 +0700

    Auth controller

--- src/main/java/com/coursemanagementsystem/controller/AuthController.java
+++ src/main/java/com/coursemanagementsystem/controller/AuthController.java
@@ -1,7 +1,29 @@
 package com.coursemanagementsystem.controller;
 
+import com.coursemanagementsystem.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "auth/login";
+    }
+
+    @GetMapping("/register")
+    public String registerForm(Model model){
+        model.addAttribute("user", new User());
+        return "auth/register";
+    }
+
+    @PostMapping("/register")
+    public String register(User user) {
+        authService.register(user);
+        return "redirect:/login";
+    }
+
 }
