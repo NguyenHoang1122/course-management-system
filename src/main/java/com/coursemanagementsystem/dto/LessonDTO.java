987b935bbc4944e62398e3a55288375686ace0c5
lichnguyen0 <fakesangmy@gmail.com>
2026-04-14 10:28:07 +0700

    feat: validation

--- src/main/java/com/coursemanagementsystem/dto/LessonDTO.java
+++ src/main/java/com/coursemanagementsystem/dto/LessonDTO.java
@@ -7,12 +7,18 @@ import lombok.Data;
 public class LessonDTO {
     private Long id;
 
-    @NotBlank(message = "Lesson title is required")
-    @Size(max = 200, message = "Lesson title must be less than 200 characters")
+    @NotBlank(message = "Tên bài học không được để trống")
+    @Size(min = 3, max = 200, message = "Tên bài học phải từ 3 đến 200 ký tự")
     private String title;
 
-    @NotBlank(message = "Video URL is required")
+    // videoUrl là tùy chọn - giảng viên có thể thêm sau
+    @Size(max = 500, message = "URL video không được vượt quá 500 ký tự")
+    @Pattern(
+        regexp = "^$|^(https?://).+",
+        message = "URL video phải bắt đầu bằng http:// hoặc https://"
+    )
     private String videoUrl;
 
+    @NotNull(message = "Khóa học không được để trống")
     private Long courseId;
 }
